92551f9de4e0bd7333aaabe1a4925d4052b37061
Quentin BRAMAS <quentin@bramas.fr>
2014-11-11 16:09:39 +0100

    display a card

--- openps/openps.py
+++ openps/openps.py
@@ -6,6 +6,11 @@ def run():
 	pygame.init()
 	DISPLAYSURF = pygame.display.set_mode((800, 600))
 	pygame.display.set_caption('OpenPS')
+
+	card = pygame.image.load("openps/resources/cards/character_jason.jpg")
+	DISPLAYSURF.blit(card, (100, 100))
+	pygame.display.update()
+
 	while True: # main game loop
 		for event in pygame.event.get():
 			if event.type == QUIT:
