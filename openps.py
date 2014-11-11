de5af89a283f4ee8a5f1f349549def1b35388fca
Quentin BRAMAS <quentin@bramas.fr>
2014-11-11 14:52:12 +0100

    test window

--- openps.py
+++ openps.py
@@ -1,3 +1,13 @@
 
+import pygame, sys
+from pygame.locals import *
 
-print("hello world");
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('OpenPS')
+while True: # main game loop
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+			pygame.display.update()
