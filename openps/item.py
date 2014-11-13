c49f843e7bb63036048edc5fb461c1bdf4e18be8
Quentin BRAMAS <quentin@bramas.fr>
2014-11-13 19:14:08 +0100

    items id are replaced by item objects

--- openps/item.py
+++ openps/item.py
@@ -2,39 +2,34 @@ import openps as ops
 
 class Item:
 
-	def __init__(self, id):
-		self.id = id
+	JERRY 	= "Jerrican"
+	VEST 	 	= "Gilet Pare-Balles"
+	MUNITION 	= "Munitions"
+	PARASITE 	= "Alerte Parasites !"
+	HEALTH_KIT 	= "Kit de Premiers Soins"
+	MAGNET   	= "Carte magnétique"
+	MACHINE_GUN = "Fusil Mitrailleur"
+	GRENADE  	= "Grenade"
+	ADRENALINE 	= "Adrénaline" 
+	GLASSES  	= "Lunette de Précision"
+	KNIFE 		= "Couteau de Combat"
+	SCANNER 	= "Scanner Corporel"
+	HOST 		= "Hôte"
+	BLOOD 		= "Sang"
+
+
+	def __init__(self, name):
+		self.name = name
 		self.weapon = False
 		self.blood=False
-		if id==1:
-			self.name = "Jerrican"
-		elif id==2:
-			self.name = "Gilet Pare-Balles"
-		elif id==3:
-			self.name = "Munitions"
-		elif id==4:
-			self.name = "Alerte Parasites !"
-		elif id==5:
-			self.name = "Kit de Premiers Soins"
-		elif id==6:
-			self.name = "Carte magnétique"
-		elif id==7:
-			self.name = "Fusil Mitrailleur"
+
+		if name in [Item.MACHINE_GUN, Item.GRENADE, Item.KNIFE]:
 			self.weapon = True
-		elif id==8:
-			self.name = "Grenade"
-			self.weapon = True
-		elif id==9:
-			self.name = "Adrénaline"
-		elif id==10:
-			self.name = "Lunette de Précision"
-		elif id==11:
-			self.name = "Couteau de Combat"
-			self.weapon = True
-		elif id==12:
-			self.name = "Scanner Corporel"
-		elif id==13:
-			self.name = "Hôte"
-		else:
-			#Chaque joueur obtient en début de partie les trois cartes Sang de sa propre couleur
-			self.name = "Sang"
+
+		if name == Item.BLOOD:
+			self.blood = True
+
+	def __str__(self):
+		return self.name
+	def __repr__(self):
+		return self.__str__()
